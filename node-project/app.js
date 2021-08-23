96bc47f5d09b66b4c11221e908dd553be72228b3
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2021-08-23 14:33:41 -0300

    Update app.js
    Ajuste para testes.

--- node-project/app.js
+++ node-project/app.js
@@ -6,6 +6,8 @@ var path = __dirname + '/views/';
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+let acessos = 0
+
 router.use(function (req,res,next) {
   console.log("/" + req.method);
   next();
@@ -19,9 +21,21 @@ router.get("/sharks",function(req,res){
   res.sendFile(path + "sharks.html");
 });
 
+router.get("/health",function(req,res){
+  if (++acessos > 5) {
+    res.status(500).send({ error: "failed"})
+  } else{
+    res.status(200).send({ health: "ok"})
+  }
+});
+
 app.use(express.static(path));
 app.use("/", router);
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+setTimeout(() => {
+
+  app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+  })
+
+}, 15000);
